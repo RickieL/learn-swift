8b9cd61f0674ec168a932d6e696fad7b0fd03784
yongfu-office <rickie622@gmail.com>
2014-10-23 18:22:00 +0800

    functions

--- swift-control-flow.playground/section-1.swift
+++ swift-control-flow.playground/section-1.swift
@@ -56,7 +56,7 @@ while square < finalSquare {
 }
 
 println("Game over!")
-*/
+
 
 do {
     // move up or down for a snake or ladder
@@ -68,6 +68,7 @@ do {
 } while square < finalSquare
 
 println("Game Over!")
+*/
 
 var temperatureInFahrenheit = 30
 if temperatureInFahrenheit <= 32 {
@@ -135,9 +136,66 @@ case let (x, y):
 // prints "on the x-axis with an x value of 2"
 
 
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let (x, y) where x == y:
+    println("(\(x), \(y)) is on the line x=y")
+case let (x, y) where x == -y:
+    println("(\(x), \(y)) is on the line x=-y")
+case let (x, y):
+    println("(\(x), \(y)) is just some arbitrary point")
+}
+
+let puzzleInput = "great minds think alike"
+var puzzleOutput = ""
+for character in puzzleInput {
+    switch character {
+    case "a", "e", "i", "o", "u", " ":
+        continue
+    default:
+        puzzleOutput.append(character)
+    }
+}
+println(puzzleOutput)
+
+
+let numberSymbol: Character = "三"
+var possibleIntegerValue: Int?
+switch numberSymbol {
+case "1", "١", "一", "๑":
+    possibleIntegerValue = 1
+case "2", "٢", "二", "๒":
+    possibleIntegerValue = 2
+case "3", "٣", "三", "๓":
+    possibleIntegerValue = 3
+case "4", "٤", "四", "๔":
+    possibleIntegerValue = 4
+default:
+    break
+}
+
+if let integerValue = possibleIntegerValue {
+    println("The integer value of \(numberSymbol) is \(integerValue)")
+} else {
+    println("An integer value could not be found for \(numberSymbol)")
+}
 
 
 
+gameLoop: while square != finalSquare {
+    if ++diceRoll == 7 { diceRoll = 1 }
+    switch square + diceRoll {
+    case finalSquare:
+        break gameLoop
+    case let newSquare where newSquare > finalSquare:
+        continue gameLoop
+    default:
+        square += diceRoll
+        square += board[square]
+    }
+}
+
+printLn("Game Passed!")
 
 
 
---
+++ swift-functions.playground/section-1.swift
@@ -0,0 +1,13 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+func sayHello(personName: String) -> String {
+    let greeting = "Hello, \(personName)"
+    return greeting
+}
+
+println(sayHello("Anna"))
+
